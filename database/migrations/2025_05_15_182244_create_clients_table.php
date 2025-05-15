f7b8287a20af184387534292e926d632d6a19d7b
gcuiza <gualbertocuizav@gmail.com>
2025-05-15 14:27:32 -0400

    add base models

---
+++ database/migrations/2025_05_15_182244_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('card_id')->unique();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->foreignId('gym_id')->constrained('gyms')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->unique(['card_id', 'gym_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
