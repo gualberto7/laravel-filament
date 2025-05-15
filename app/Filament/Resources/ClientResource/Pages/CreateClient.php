e420ce976da0a64751031e65e85c7405cc7717a7
gcuiza <gualbertocuizav@gmail.com>
2025-05-15 14:49:58 -0400

    clients resource

---
+++ app/Filament/Resources/ClientResource/Pages/CreateClient.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ClientResource\Pages;
+
+use App\Filament\Resources\ClientResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateClient extends CreateRecord
+{
+    protected static string $resource = ClientResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['gym_id'] = auth()->user()->gym->id;
+        
+        return $data;
+    }
+}
