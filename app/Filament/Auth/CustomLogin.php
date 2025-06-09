543d9dbd948bfb019819a77f0e4e20cafcf88916
gcuiza <gualbertocuizav@gmail.com>
2025-06-09 10:55:05 -0400

    add login with username

---
+++ app/Filament/Auth/CustomLogin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Auth;
+
+use Filament\Pages\Auth\Login;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Validation\ValidationException;
+
+class CustomLogin extends Login
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        TextInput::make('username')
+                            ->label('Username')
+                            ->required(),
+                        $this->getPasswordFormComponent(),
+                        $this->getRememberFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        return [
+            'username' => $data['username'],
+            'password' => $data['password'],
+        ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.username' => __('filament-panels::pages/auth/login.messages.failed'),
+        ]);
+    }
+}
