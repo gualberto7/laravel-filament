38b689a88aaacdd0a8d58cd7784edf0e65b5c5cc
gcuiza <gualbertocuizav@gmail.com>
2025-06-12 10:45:00 -0400

    base gym resource

---
+++ app/Filament/SuperAdmin/Resources/GymResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\SuperAdmin\Resources;
+
+use App\Filament\SuperAdmin\Resources\GymResource\Pages;
+use App\Filament\SuperAdmin\Resources\GymResource\RelationManagers;
+use App\Models\Gym;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GymResource extends Resource
+{
+    protected static ?string $model = Gym::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('address')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('address'),
+                Tables\Columns\TextColumn::make('phone'),
+                Tables\Columns\TextColumn::make('owner.name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGyms::route('/'),
+            'create' => Pages\CreateGym::route('/create'),
+            'edit' => Pages\EditGym::route('/{record}/edit'),
+        ];
+    }
+}
