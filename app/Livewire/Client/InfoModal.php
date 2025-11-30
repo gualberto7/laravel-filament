828dbb620862259db722d65c2a1d28b1bebd51b0
gcuiza <gualbertocuizav@gmail.com>
2025-11-30 11:52:33 -0400

    add key_register value

--- app/Livewire/Client/InfoModal.php
+++ app/Livewire/Client/InfoModal.php
@@ -3,7 +3,7 @@
 namespace App\Livewire\Client;
 
 use App\Models\Client;
-//use App\Models\Gym;
+use App\Models\Gym;
 use Livewire\Component;
 use Livewire\Attributes\On;
 use Filament\Notifications\Notification;
@@ -13,15 +13,14 @@ class InfoModal extends Component
     public $client;
     public $subscription;
     public $key_number;
-    /*public $gym;
+    public $gym;
     public $register_key;
 
     public function mount()
     {
         $this->gymId = auth()->user() ? auth()->user()->getCurrentGymId() : null;
         $this->register_key = Gym::findOrFail($this->gymId)->getPreference('register_key');
-        var_dump($this->register_key);
-    }*/
+    }
 
     #[On('addCheckin')]
     public function addCheckin($client)
